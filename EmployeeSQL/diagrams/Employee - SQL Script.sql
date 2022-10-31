25dc4b6b2fa5b5908df2316cd880d6003b35be66
Cheila Bergamini <cheilamaria@gmail.com>
2022-10-31 09:39:53 +0000

    Renaming files and updated version of the QuickDBD diagram files.

--- EmployeeSQL/diagrams/Employee - SQL Script.sql
+++ EmployeeSQL/diagrams/Employee - SQL Script.sql
@@ -17,8 +17,8 @@ CREATE TABLE "dept_emp" (
 );
 
 CREATE TABLE "dept_manager" (
-    "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR(5)   NOT NULL
+    "dept_no" VARCHAR(5)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL
 );
 
 CREATE TABLE "employee" (
@@ -28,7 +28,10 @@ CREATE TABLE "employee" (
     "first_name" VARCHAR(30)   NOT NULL,
     "last_name" VARCHAR(30)   NOT NULL,
     "sex" VARCHAR(1)   NOT NULL,
-    "hire_date" DATE   NOT NULL
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employee" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "salary" (
@@ -50,15 +53,15 @@ REFERENCES "employee" ("emp_no");
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "department" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employee" ("emp_no");
-
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "department" ("dept_no");
 
-ALTER TABLE "employee" ADD CONSTRAINT "fk_employee_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "salary" ("emp_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
 
 ALTER TABLE "employee" ADD CONSTRAINT "fk_employee_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "title" ("title_id");
+
+ALTER TABLE "salary" ADD CONSTRAINT "fk_salary_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
 
